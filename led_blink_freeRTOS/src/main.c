3cf6277e168128c21c1066ea184683861d7da03f
earnest-gakure <earnestkihara7@gmail.com>
2026-03-13 02:27:00 +0300

    RBG pattern blinking

--- led_blink_freeRTOS/src/main.c
+++ led_blink_freeRTOS/src/main.c
@@ -1,37 +1,62 @@
 #include "TM4C123GH6PM.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include <time.h>
 
-void led_task(void *pvParameters)
-{
-    // Enable clock for GPIOF
-    SYSCTL->RCGCGPIO |= (1 << 5);
-    while ((SYSCTL->PRGPIO & (1 << 5)) == 0);
-
-    // Unlock GPIOF (required for PF0; harmless for other pins)
+void gpio_init(){
+    SYSCTL->RCGCGPIO |= (1<<5);
     GPIOF->LOCK = 0x4C4F434B;
     GPIOF->CR   = 0xFF;
 
-    // Configure PF1 (Red LED)
-    GPIOF->AMSEL &= ~(1 << 1);   // disable analog
-    GPIOF->AFSEL &= ~(1 << 1);   // GPIO function, not alternate
-    GPIOF->DIR   |=  (1 << 1);   // output
-    GPIOF->DEN   |=  (1 << 1);   // digital enable
+    GPIOF->DIR |= (1<<1) | (1<<2) | (1<<3);
+    GPIOF->DEN |= (1<<1) | (1<<2) | (1<<3);
 
-    // Re-lock
-    GPIOF->CR   = 0x00;
-    GPIOF->LOCK = 0;
+}
 
+void red_led_task(void *pvParameters)
+{
+    // Enable clock for GPIOF
+    gpio_init();
+    vTaskDelay(pdMS_TO_TICKS(200));
     while (1)
     {
-        GPIOF->DATA ^= (1 << 1);
-        vTaskDelay(pdMS_TO_TICKS(500));
+        GPIOF->DATA |= (1 << 1);
+        vTaskDelay(pdMS_TO_TICKS(300));
+        GPIOF->DATA &= ~(1<<1);
+        vTaskDelay(pdMS_TO_TICKS(300));
+
+    }
+}
+void green_led_task(void *pvParameters){
+   gpio_init();
+   //vTaskDelay(pdMS_TO_TICKS(200));
+
+    while(1){
+        GPIOF->DATA |= (1<<3);
+        vTaskDelay(pdMS_TO_TICKS(600));
+        GPIOF->DATA &= ~(1<<3);
+        vTaskDelay(pdMS_TO_TICKS(600));
+    }
+}
+void blue_led_task(void *pvParameters){
+   gpio_init();
+   vTaskDelay(pdMS_TO_TICKS(200));
+   
+    while(1){
+        GPIOF->DATA |= (1<<2);
+        vTaskDelay(pdMS_TO_TICKS(900));
+        GPIOF->DATA &= ~(1<<2);
+        vTaskDelay(pdMS_TO_TICKS(900));
     }
 }
 
+
 int main(void)
 {
-    xTaskCreate(led_task, "LED", 256, NULL, 1, NULL);
+    xTaskCreate(red_led_task,  "RED  LED", 256, NULL, 1, NULL);
+    xTaskCreate(blue_led_task,"BLUE LED",256, NULL, 1 ,NULL );
+    xTaskCreate(green_led_task,"GREEN LED",256, NULL, 1 ,NULL );
+    
     vTaskStartScheduler();
     while (1);
 }
