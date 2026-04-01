4591b3fd101f340a57a68a90177656b60d998490
earnest-gakure <earnestkihara7@gmail.com>
2026-04-01 10:06:32 +0300

    software timer debounce

--- debounce_software_timer/src/main.c
+++ debounce_software_timer/src/main.c
@@ -27,30 +27,43 @@ void gpio_init(){
 
 }
 /* callback function for buttton 1  timer*/
-void vtimer1_callback(TimerHandle_t xTimer)
+void vtimer_callback(TimerHandle_t xTimer)
 {
-    
+    uint32_t id = (uint32_t)pvTimerGetTimerID(xTimer);
+    if (id ==  1)
+    {
         if ((GPIOF->DATA & (1<<4)) == 0x00)
         {
+            
             GPIOF->DATA ^= (1<<1);
         }
 
-}
-//callback function for button 2 timer
-void vtimer2_callback(TimerHandle_t xTimer)
-{
-    if ((GPIOF->DATA  & (1<<0)) == 0x00)
-    {
-            GPIOF->DATA ^= (1<<2);
     }
+    
+    if (id == 2)
+    {
+        
+        if ((GPIOF->DATA  & (1<<0)) == 0x00)
+        {
+                GPIOF->DATA ^= (1<<2);
+        }
+    }
+       
+
+
 }
+
 /*task to poll the state of the buttons*/
 void vbuttonTask(void *pvParameter){
     while (1)
     {  
         if (((GPIOF->DATA  & (1<<4)) == 0x00) && (button1_state == 1))
         {
+            uint32_t id = 1;
             button1_state = 0;
+            //set timer ID
+            vTimerSetTimerID(xtimer1, (void *)id);
+            
             //reset timer 
             xTimerReset(xtimer1,0);
         }
@@ -61,14 +74,18 @@ void vbuttonTask(void *pvParameter){
 
         if (((GPIOF->DATA & (1<<0)) == 0x00) && (button2_state == 1))
         {
+            uint32_t id = 2;
             button2_state = 0;
+            //set timer id
+            vTimerSetTimerID(xtimer2, (void *)id);
+            
             //reset timer 
             xTimerReset(xtimer2,0);
         } else if (((GPIOF->DATA & (1<<0)) != 0x00))
         {
             button2_state = 1;
         }    
-        vTaskDelay(pdMS_TO_TICKS(10));
+        //vTaskDelay(pdMS_TO_TICKS(10));
     }
     
 
@@ -78,8 +95,8 @@ int main(void)
 {
     gpio_init();
     //create timer
-    xtimer1 = xTimerCreate("button 1", pdMS_TO_TICKS(50), pdFALSE, 0, vtimer1_callback);
-    xtimer2 = xTimerCreate("button 2", pdMS_TO_TICKS(50), pdFALSE, 0, vtimer2_callback);
+    xtimer1 = xTimerCreate("button 1", pdMS_TO_TICKS(50), pdFALSE, 0, vtimer_callback);
+    xtimer2 = xTimerCreate("button 2", pdMS_TO_TICKS(50), pdFALSE, 0, vtimer_callback);
 
 
     xTaskCreate(vbuttonTask, "buttons", 256, NULL, 1, NULL);
